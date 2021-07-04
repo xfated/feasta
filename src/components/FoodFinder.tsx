8bd964871c82bb20961dc10faa3f7ebef8cc996c
Tay Kai Yang <taykaiyang556@gmail.com>
2021-07-04 15:49:03 +0800

    added form

--- src/components/FoodFinder.tsx
+++ src/components/FoodFinder.tsx
@@ -2,10 +2,42 @@ import React, { useState } from 'react';
 import './FoodFinder.css';
 import QueryForm from './QueryForm';
 
+// Define interfaces for our results
+interface QueryResult {
+}
+
+// type QueryType = "Random" | "Semantic";
+export interface Query {
+    postal: string;
+    topk: number;
+    querytype: string;
+    query: string;
+}
+
+var url:string = "http://127.0.0.1:8000";
+
 const FoodFinder = () => {
+    
+    const [results, setResults] = useState<Array<null | QueryResult>>()
+    
+    // Function to make request and store results
+    const handleQuery = (values: Query) => {
+        alert("Submission:\n" + JSON.stringify(values))
+        return
+    }   
+
     return(
-        <>
-            <QueryForm />
+        <>  
+            <div className="divider"></div>
+            <div className="section-header col-12 text-center">
+                <div className="col-12 flex flex-horizontal-center mb-3">
+                    <h1 className="mb-0 col-6 col-sm-4 header-underline">Where to feast?</h1>
+                </div>
+                <p>Some suggestions for when you're out of options.</p>
+            </div>
+            <div className="query-container pb-5">
+                <QueryForm handleQuery={handleQuery}/>
+            </div>
         </>
     )
 }
--- src/components/QueryForm.tsx
+++ src/components/QueryForm.tsx
@@ -1,27 +1,99 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Row, Button, Label, Form, FormGroup, Input, FormFeedback, FormText } from 'reactstrap';
+import './QueryForm.css';
+import { Query } from './FoodFinder';
 
-const QueryForm = () => {
+interface PropsFunction {
+    handleQuery: (values: Query) => void
+}
 
-    // Data for controlled form. TO make query
+const QueryForm = ({ handleQuery }: PropsFunction) => {
+    useEffect(() => {
+        console.log("Mounted")
+    }, [])
+
     const [query, setQuery] = useState({
-        query: "",
         postal: "",
         topk: 0,
-    });
-    type queryMode = "random" | "embed";
-    const [queryMode, setQueryMode] = useState<queryMode>("random");
+        querytype: "Random",
+        query: ""
+    })
 
-    const resetForm = () => {
-        setQuery({
-            query: "",
-            postal: "",
-            topk: 0,
-        })
+    const [postalValid, setPostalValid] = useState(true)
+    const validatePostal = (postal: string) => {
+        if (postal.length === 0) {
+            setPostalValid(true);
+            return
+        }
+        const postalRe = /^\d{3}$/;
+        const validate = postalRe.test(postal);
+        setPostalValid(validate);
+    }
+
+    const submitQuery = (e: React.FormEvent) => {
+        e.preventDefault()
+        handleQuery(query)
     }
 
     return(
         <>
-            <p>This is my test</p>
+            <div className="container">
+                <Form onSubmit = {(e) => submitQuery(e)}>
+                    <Row>
+                        <FormGroup className="col-12 col-md-4 mt-2 mt-md-0">
+                            <Label htmlFor="postal">&nbsp;Postal Code</Label>
+                            <Input type="text" id="postal" name="postal" value={query.postal}
+                                placeholder="Postal Code"
+                                invalid={!postalValid}
+                                onChange={(e) => {
+                                    validatePostal(e.target.value);
+                                    setQuery({...query,
+                                        postal:e.target.value});
+                                }}/>
+                            <FormFeedback>Invalid postal code. Please input 3 digits or simply leave the field empty</FormFeedback>
+                            <FormText>First 3 digits of postal code area you wish to search</FormText>
+                        </FormGroup>
+                        <FormGroup className="col-12 col-md-4 mt-2 mt-md-0">
+                            <Label htmlFor="topk">&nbsp;Number of Results</Label>
+                            <Input type="number" id="topk" name="topk" min="0" value={query.topk}
+                                placeholder="Number of results to retrieve"
+                                onChange={(e) => setQuery({...query,
+                                                        topk:parseInt(e.target.value)}
+                                )}/>
+                            <FormText>Number of restaurants to return</FormText>
+                        </FormGroup>
+                        <FormGroup className="col-12 col-md-4 mt-2 mt-md-0">
+                            <Label htmlFor="querytype">&nbsp;Query Type</Label>
+                            <Input type="select" id="querytype" name="querytype" value={query.querytype}
+                                placeholder="Random or Semantic search"
+                                onChange={(e) => setQuery({
+                                                    ...query,
+                                                    querytype:e.target.value}
+                                                )}>
+                                <option>Random</option>
+                                <option>Semantic</option>
+                            </Input>
+                            <FormText>{`Semantic: Search restaurants that are similar to your query | Random: Returns restaurants randomly`}</FormText> 
+                        </FormGroup>
+                    </Row>
+                    <Row className="mt-2">
+                        <FormGroup>
+                            <Label htmlFor="query">&nbsp;Query</Label>
+                            <Input type="textarea" id="query" name="query" value={query.query}
+                                placeholder="Your query here"
+                                onChange={(e) => setQuery({
+                                                    ...query,
+                                                    query:e.target.value}
+                                                )}/>
+                        </FormGroup>
+                    </Row>
+                    <Row className="mt-3 flex flex-horizontal-center">
+                        <Button outline type="submit" value="submit" color="secondary" className="mt-3 col-3">
+                            Submit
+                        </Button>
+                        </Row>
+                </Form>
+            </div>
         </>
     )
 }
