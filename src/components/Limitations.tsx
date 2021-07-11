36409263ae2fc49a2aff31af8546c8905d70dd43
Tay Kai Yang <taykaiyang556@gmail.com>
2021-07-11 18:06:55 +0800

    fixed hte overlat

--- src/components/Limitations.tsx
+++ src/components/Limitations.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './Limitations.css';
+import { UncontrolledPopover, PopoverBody } from 'reactstrap';
 
 const Limitations = () => {
     const [showInfo, setShowInfo] = useState(false);
@@ -34,16 +35,21 @@ const Limitations = () => {
     return (
         <div className="w-100 flex flex-horizontal-center">
             <div className="wrapper" >
-                <div className="disclaimer flex flex-horizontal-center">  
+                <div className={`disclaimer flex flex-horizontal-center` + `${showInfo ? ' disclaimer-bg' : ''}`}>
                     <img src={"/assets/images/disclaimer.jpg"} className="disclaimer-img">
                     </img>
                     <div className="disclaimer-text-box">
-                        <h4 className="display-4">Disclaimer</h4>
+                        <h4 className="display-4">Setting Expectations</h4>
                     </div>
                 </div>
                 {   showInfo &&
-                    <div className="info">
-                        lo
+                    <div className="info flex flex-horizontal-center">
+                         <p id="time-info"><small>P.S. Semantic search only available from 9am to 9pm.</small></p>
+                        <UncontrolledPopover trigger="hover click" placement="bottom" target="time-info">
+                            <PopoverBody>
+                                Because hosting is expensive.
+                            </PopoverBody>
+                        </UncontrolledPopover>
                     </div>
                 }
             </div>
